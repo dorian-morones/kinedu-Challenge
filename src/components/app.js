2b64d22ec6fafafb812e42792c124e714fbd7747
Dorian Morones <dorianm393@gmail.com>
2020-01-24 13:33:38 -0600

    Environment Setup

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>
+         Kinedu Challenge
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
